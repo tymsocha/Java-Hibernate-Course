ecd1819e0b56a9ff97752574cb01d3d97a84f3ed
tymsocha <tymoteusz.socha@outlook.com>
2020-01-06 13:41:19 +0100

    Project created

---
+++ src/main/java/com/myprojects/javahibernatecourse/JavaHibernateCourseApplication.java
@@ -0,0 +1,13 @@
+package com.myprojects.javahibernatecourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaHibernateCourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaHibernateCourseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/myprojects/javahibernatecourse/JavaHibernateCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myprojects.javahibernatecourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaHibernateCourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
